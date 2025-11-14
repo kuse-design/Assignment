9478ac0221a0234fafd8bf69d26a0b91140cca7e
Kuse_design <0luwakusejerry1@gmail.com>
2025-11-14 06:27:48 +0100

    Assignment

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+number = int(input(" Enter any number to kmow if its even or odd number : "))
+if (number % 2 == 0):
+    print("the number is an even number" )
+
+if(number % 2 == 1):
+    print("the number is an odd number")
