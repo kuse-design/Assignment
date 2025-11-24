da070befca7416d35872a268378413c8b53527c7
Kuse_design <0luwakusejerry1@gmail.com>
2025-11-24 09:15:09 +0100

    cornflakes

---
+++ python_assignment/cornflakes/multiplication.py
@@ -0,0 +1,3 @@
+user_input = int(input("Enter a number : "))
+for number in range(1, 11):
+    print(f"{user_input} x {number} = {number*user_input}")
---
+++ python_assignment/cornflakes/student.py
@@ -0,0 +1,9 @@
+failed_mark = 0
+passed_mark = 0
+
+for number in range (0, 15):
+    teacher_user = int(input("Enter score number : "))
+passed_mark += 1
+failed_mark += 1
+if passed_mark > 45:
+        print(teacher_user)
